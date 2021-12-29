f485659830ee2f1f8a1c55d2f9fe35cbfe216a28
hvhvuu <63218769+hvhvuu@users.noreply.github.com>
2021-12-28 21:47:06 -0800

    stopwatch done need css

--- screens/statistics.js
+++ screens/statistics.js
@@ -1,44 +1,70 @@
 import * as React from 'react';
-import { View, Text, Button, StyleSheet} from 'react-native'
+import "./statisticsComponents/statistics.css"
+import {StyleSheet} from 'react-native';
 
+/*
 const statistics = ({navigation}) => {
     return (
         <View style={styles.container}>
             <Text style={styles.text}>Statistics</Text>
         </View>
     )
-}
+} */
 
-// stopwatch widget
+// functional stopwatch
+const statistics = () => {
+    const [time, setTime] = React.useState(0);
+    const [timerOn, setTimerOn] = React.useState(false);
+
+    React.useEffect(() => {
+        let interval = null;
+
+        if (timerOn) {
+            interval = setInterval(() => {
+                setTime((prevTime) => prevTime + 10);
+            }, 10);
+        }
+        else if (!timerOn) {
+            clearInterval(interval);
+        }
+
+        return () => clearInterval(interval);
+
+    }, [timerOn]);
+
+    return (
+        <div className="container">
+            <div className="Timers">
+                <h2>Exercise</h2>
+                <div id="display">
+                    <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+                    <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+                    <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+                </div>
+                <div id="buttons">
+                    {!timerOn && time === 0 && (
+                        <button onClick={() => setTimerOn(true)}>Start</button>
+                    )}
+                    {timerOn && <button onClick={() => setTimerOn(false)}>Stop</button>}
+                    {!timerOn && time > 0 && (
+                        <button onClick={() => setTime(0)}>Reset</button>
+                    )}
+                    {!timerOn && time > 0 && (
+                        <button onClick={() => setTimerOn(true)}>Resume</button>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
         backgroundColor: '#0C0C0E',
-        alignItems: 'center',
-        justifyContent: 'center',
     },
-    text: {
-        color: 'white',
-    },
-    header: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    footer: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-    },
-    timer: {
-        fontSize: 60,
-    },
-    buttonContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-});
+})
 
 export default statistics;
